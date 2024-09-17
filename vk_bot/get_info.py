bbe001b2a2ffc904d71ae10b1327818c166d509d
Sergey Tormozov <web@stormozov.ru>
2024-09-17 19:33:30 +0300

    feat: Add user search by parameters
    Added the "search_users" method for the "UserInfoRetriever" class, which searches for users by parameters and returns a list from dictionaries.

--- vk_bot/get_info.py
+++ vk_bot/get_info.py
@@ -32,5 +32,35 @@ class UserInfoRetriever:
     def get_user_photos(self):
         pass
 
-    def search_users(self):
+    def search_users(
+            self,
+            count: int = 12,
+            age_from: int = 18,
+            age_to: int = 50,
+            city_id: int = 1,
+            sex: int = 1,
+            status: int = 6,
+            has_photo: int = 1
+    ) -> list[dict] | None:
+        try:
+            params = {
+                "access_token": self.TOKEN,
+                "v": self.vk_api_version,
+                "count": count,
+                "age_from": age_from,
+                "age_to": age_to,
+                "city_id": city_id,
+                "sex": 2 if sex == 1 else 1,
+                "status": status,
+                "has_photo": has_photo,
+                "fields": "city, bdate"
+            }
+            response = requests.get(f"{self.URL}users.search", params)
+
+            return response.json()["response"]["items"]
+
+        except requests.exceptions.RequestException:
+            return None
+
+    def save_received_users(self):
         pass
